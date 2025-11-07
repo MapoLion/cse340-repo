d8506af5e8ad608503ad64680c245512c99e9c1d
John Helaman Chan <johnhelaman10@gmail.com>
2025-11-07 19:01:55 +0800

    SQL Database Added

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340databasejhc;
+    OWNER TO jhchancs340;
 
 
 CREATE TABLE public.classification (
