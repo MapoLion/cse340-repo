a76c716bb0a1c04528d4ad7930b1dd390eb471b9
John Helaman Chan <johnhelaman10@gmail.com>
2025-11-17 20:02:37 +0800

    Registration is now processable

--- server.js
+++ server.js
@@ -19,6 +19,8 @@
   const utilities = require("./utilities/");
   const session = require("express-session")
   const pool = require('./database/')
+  const bodyParser = require("body-parser")
+
 
 
   /* ***********************
@@ -34,6 +36,9 @@
     saveUninitialized: true,
     name: 'sessionId',
   }))
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
   // Express Messages Middleware
   app.use(require('connect-flash')())
